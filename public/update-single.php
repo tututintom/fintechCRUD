1ea03aa63c589a71f0c64ba9b567d182fa4b1f9b
tonmaiwood <maimo2706@gmail.com>
2019-11-25 01:02:59 +0700

    add delete page and add csrf

--- public/update-single.php
+++ public/update-single.php
@@ -1,12 +1,13 @@
 <?php
 /**
-  * Use an HTML form to edit an entry in the
-  * users table.
-  *
-  */
+ * Use an HTML form to edit an entry in the
+ * users table.
+ *
+ */
 require "../config.php";
 require "../common.php";
 if (isset($_POST['submit'])) {
+  if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
   try {
     $connection = new PDO($dsn, $username, $password, $options);
     $user =[
@@ -18,24 +19,23 @@ if (isset($_POST['submit'])) {
       "location"  => $_POST['location'],
       "date"      => $_POST['date']
     ];
-
-    $sql = "UPDATE users
-            SET id = :id,
-              firstname = :firstname,
-              lastname = :lastname,
-              email = :email,
-              age = :age,
-              location = :location,
-              date = :date
+    $sql = "UPDATE users 
+            SET id = :id, 
+              firstname = :firstname, 
+              lastname = :lastname, 
+              email = :email, 
+              age = :age, 
+              location = :location, 
+              date = :date 
             WHERE id = :id";
-
+  
   $statement = $connection->prepare($sql);
   $statement->execute($user);
   } catch(PDOException $error) {
       echo $sql . "<br>" . $error->getMessage();
   }
 }
-
+  
 if (isset($_GET['id'])) {
   try {
     $connection = new PDO($dsn, $username, $password, $options);
@@ -44,7 +44,7 @@ if (isset($_GET['id'])) {
     $statement = $connection->prepare($sql);
     $statement->bindValue(':id', $id);
     $statement->execute();
-
+    
     $user = $statement->fetch(PDO::FETCH_ASSOC);
   } catch(PDOException $error) {
       echo $sql . "<br>" . $error->getMessage();
@@ -58,16 +58,17 @@ if (isset($_GET['id'])) {
 <?php require "templates/header.php"; ?>
 
 <?php if (isset($_POST['submit']) && $statement) : ?>
-  <?php echo escape($_POST['firstname']); ?> successfully updated.
+	<blockquote><?php echo escape($_POST['firstname']); ?> successfully updated.</blockquote>
 <?php endif; ?>
 
 <h2>Edit a user</h2>
 
 <form method="post">
+    <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
     <?php foreach ($user as $key => $value) : ?>
       <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
-      <input type="text" name="<?php echo $key; ?>" id="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'id' ? 'readonly' : null); ?>>
-    <?php endforeach; ?>
+	    <input type="text" name="<?php echo $key; ?>" id="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'id' ? 'readonly' : null); ?>>
+    <?php endforeach; ?> 
     <input type="submit" name="submit" value="Submit">
 </form>
 
